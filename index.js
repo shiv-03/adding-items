d11efac8afdda2a6199f9ea26112d7bd9f379cce
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-09-29 02:22:36 +0530

    Changes made in CSS and a little tweek in js of add function for more interactivity

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function add(){
-    var input=document.getElementById("text");
-    var li=document.createElement("LI");
-    li.innerHTML=input.value;
-    input.value=""
-    document.getElementById("list").appendChild(li);
+let click = true
 
+function add() {
+    let input = document.getElementById("text")
+    if (click) {
+        document.getElementById('list').innerHTML = ''
+    }
+    if (input.value == "") {
+        alert("Input field is empty")
+        return
+    }
+    click = false
+    let li = document.createElement("li")
+    li.innerHTML = input.value
+    input.value = ""
+    document.getElementById("list").appendChild(li)
 }
