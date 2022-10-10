b76bd3a093ebb3c27927700de76ee74040c98f99
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-10-10 21:10:48 +0530

    Added delete button so that added items can be removed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let click = true
-
+let count = 0
 function add() {
     let input = document.getElementById("text")
     
@@ -12,7 +12,24 @@ function add() {
     }
     click = false
     let li = document.createElement("li")
-    li.innerHTML = input.value
+    li.setAttribute('class','list-group')
+    li.setAttribute('id', count)
+    li.innerHTML = input.value + `<span onclick=deleteFun(${count})>Delete</span>`
     input.value = ""
+    count++
     document.getElementById("list").appendChild(li)
+}
+
+function deleteFun(id) {
+
+    let list = document.getElementById("list")
+    for(let i   = 0; i < list.children.length; i++) {
+        if(list.children[i].getAttribute('id') == id) {
+            list.removeChild(list.children[i]);
+        }
+    }
+
+    if(list.children.length == 0) {
+        list.innerHTML= '<li>This is a demo child 1</li>'
+    }
 }
